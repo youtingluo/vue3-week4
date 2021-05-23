acf04440225b28e341669f9293e655c568c7a2bc
youting <youting.gmail.com>
2021-05-23 18:08:04 +0800

    getData

--- js/all.js
+++ js/all.js
@@ -1,91 +1,24 @@
-const userName = document.getElementById('userName');
-const password = document.getElementById('password');
-const loginBtn = document.getElementById('login');
-const itemList = document.getElementById('itemList');
-const inputName = document.getElementById('inputName');
-const inputPrice = document.getElementById('inputPrice');
-const inputSale = document.getElementById('inputSale');
-const addProduct = document.querySelector('.add');
-const url = 'https://vue3-course-api.hexschool.io';
-const path = 'youting';
-
-const App = {
-  data: [],
-  // 取得產品
-  getProducts() {
-    axios.get(`${url}/api/${path}/products`).then((res) => {
-      console.log(res);
-      if (res.data.success) {
-        this.data = res.data.products;
-        this.render();
-      } else {
-        alert(`${res.data.message}`);
-      }
-    });
-  },
-  // 新增產品
-  addProduct() {
-    const product = {
-      data: {
-        title: inputName.value,
-        category: '測試',
-        origin_price: Math.abs(parseInt(inputPrice.value)),
-        price: Math.abs(parseInt(inputSale.value)),
-        unit: '個',
-        is_enabled: 1,
-      },
+let myModal = new bootstrap.Modal(document.getElementById('productModal'));
+const app = {
+  data() {
+    return {
+      url: 'https://vue3-course-api.hexschool.io',
+      path: 'youting',
+      products: [],
     };
-    axios.post(`${url}/api/${path}/admin/product`, product).then((res) => {
-      if (res.data.success) {
-        this.getProducts();
-        alert('新增產品成功');
-        inputName.value = '';
-        inputPrice.value = '';
-        inputSale.value = '';
-      } else {
-        alert(`${res.data.message}`);
-      }
-    });
   },
-  // 刪除產品
-  deleteProduct(evt) {
-    if (!evt.target.dataset.id) {
-      return;
-    }
-    if (!window.confirm('確定要刪除嗎?')) return;
-    const id = evt.target.dataset.id;
-    axios.delete(`${url}/api/${path}/admin/product/${id}`).then((res) => {
-      if (res.data.success) {
-        this.getProducts();
-      } else {
-        alert(`${res.data.message}`);
-      }
-    });
+  methods: {
+    openModal() {
+      myModal.show();
+    },
+    getProducts() {
+      axios.get(`${this.url}/api/${this.path}/admin/products`).then((res) => {
+        console.log(this.products, res.data.products);
+        this.products = res.data.products;
+      });
+    },
   },
-  // 渲染畫面
-  render() {
-    let str = '';
-    this.data.forEach((item) => {
-      str += `<tr>
-      <td>${item.title}</td>
-      <td>${item.origin_price}</td>
-      <td>${item.price}</td>
-      <td>
-        <div class="item">
-          <input type="checkbox" name="" id="group" checked/>
-          <label for="group">啟用</label>
-        </div>
-      </td>
-      <td>
-        <div class="del_btn" data-id="${item.id}">X</div>
-      </td>
-    </tr>`;
-    });
-    itemList.innerHTML = str;
-  },
-  // init
-  created() {
-    //存入header
+  mounted() {
     const token = document.cookie.replace(
       /(?:(?:^|.*;\s*)ytToken\s*\=\s*([^;]*).*$)|^.*$/,
       '$1'
@@ -94,6 +27,4 @@ const App = {
     this.getProducts();
   },
 };
-App.created();
-addProduct.addEventListener('click', App.addProduct.bind(App));
-itemList.addEventListener('click', App.deleteProduct.bind(App));
+Vue.createApp(app).mount('#app');
--- js/login.js
+++ js/login.js
@@ -1,34 +1,62 @@
-const username = document.querySelector('#username');
-const password = document.querySelector('#password');
-const form = document.querySelector('#form');
+Vue.createApp({
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    login() {
+      const url = 'https://vue3-course-api.hexschool.io';
+      const user = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(`${url}/admin/signin`, user)
+        .then((res) => {
+          if (res.data.success) {
+            console.log(res);
+            const { token, expired } = res.data;
+            document.cookie = `ytToken=${token}; expires=${new Date(
+              expired
+            )}; path=/`;
+            window.location = 'product.html';
+          }
+        })
+        .catch((res) => {
+          console.log(res);
+        });
+    },
+  },
+}).mount('#app');
 
-function login(event) {
-  event.preventDefault();
-  // 以下為示範程式碼
-  const url = 'https://vue3-course-api.hexschool.io';
-  const path = 'youting';
-  const user = {
-    username: username.value,
-    password: password.value,
-  };
-  axios
-    .post(`${url}/admin/signin`, user)
-    .then((response) => {
-      if (response.data.success) {
-        const { token, expired } = response.data;
-        // 寫入 cookie token
-        // expires 設置有效時間
-        document.cookie = `ytToken=${token}; expires=${new Date(
-          expired
-        )}; path=/`;
-        window.location = 'product.html';
-      } else {
-        alert(response.data.message);
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
+// function login(event) {
+//   event.preventDefault();
+//   // 以下為示範程式碼
+//   const url = 'https://vue3-course-api.hexschool.io';
+//   const user = {
+//     username: username.value,
+//     password: password.value,
+//   };
+//   axios
+//     .post(`${url}/admin/signin`, user)
+//     .then((response) => {
+//       if (response.data.success) {
+//         const { token, expired } = response.data;
+//         // 寫入 cookie token
+//         // expires 設置有效時間
+//         document.cookie = `ytToken=${token}; expires=${new Date(
+//           expired
+//         )}; path=/`;
+//         window.location = 'product.html';
+//       } else {
+//         alert(response.data.message);
+//       }
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// }
 
-form.addEventListener('submit', login);
+// form.addEventListener('submit', login);
